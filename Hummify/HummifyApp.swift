8695bf8f104379a9a35ed77da2fe17c0753d4c31
Calvinc377 <calvinchristiano377.cc@gmail.com>
2024-06-06 08:26:57 +0700

    Initial Commit

---
+++ Hummify/HummifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HummifyApp.swift
+//  Hummify
+//
+//  Created by Calvin Christiano on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HummifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
